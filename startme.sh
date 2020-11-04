4c02c9477f1428ab88d9bfd1f1a55f654ec21633
René Bod <rene.bod@mac.com>
2020-11-04 16:48:40 +0100

    Added Proxy challenge

--- startme.sh
+++ startme.sh
@@ -4,5 +4,6 @@ docker-compose build
 docker-compose run --rm bookstore django-admin startproject project .
 docker-compose run --rm bookstore python manage.py startapp bookstore_api
 # docker-compose run --rm bookstore python manage.py createsuperuser
-docker-compose up -d
+docker-compose up -d --remove-orphans
+# docker-compose -f docker-compose_proxy.yml up -d --remove-orphans
 docker-compose logs -f
