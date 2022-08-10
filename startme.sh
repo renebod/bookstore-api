e201e27ee82e0001b45e759d983fe5fd8e381e95
René Bod <rene.bod@minbzk.nl>
2022-08-10 11:55:58 +0200

    Auto run notebooks

--- startme.sh
+++ startme.sh
@@ -1,11 +1,21 @@
 #!/usr/bin/env bash
 
 docker-compose build
+docker-compose down -v
+docker-compose run --rm bookstore rm -Rf manage.py project bookstore manage.py db.sqlite3
 docker-compose run --rm bookstore django-admin startproject project .
 docker-compose run --rm bookstore python manage.py startapp bookstore
 # docker-compose run --rm bookstore python manage.py createsuperuser
 docker-compose up -d --remove-orphans
 # docker-compose -f docker-compose_proxy.yml up -d --remove-orphans
+
+# docker-compose run --rm jupyter python run_notebooks.py '01 Automation Challenge Book API - Settings.ipynb'
+# docker-compose run --rm jupyter python run_notebooks.py '02 Automation Challenge Book API - Database.ipynb'
+# docker-compose run --rm jupyter python run_notebooks.py '03 Automation Challenge Book API - Models.ipynb'
+# docker-compose run --rm jupyter python run_notebooks.py  '04 Automation Challenge Book API - Serialization.ipynb'
+# docker-compose run --rm jupyter python run_notebooks.py  '05 Automation Challenge Book API - URLs en Views.ipynb'
+# docker-compose run --rm jupyter python run_notebooks.py  '06 Automation Challenge Book API - Pagination.ipynb'
+
 docker-compose logs -f
 
 # Visualise database model
